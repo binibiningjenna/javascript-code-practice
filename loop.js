694b4a63b34f5f108caed02a737b56f78fd3cc3b
Jenna Miles Reyes <atienzajennamiles@gmail.com>
2025-07-26 00:33:27 +0800

    practice looping

--- loop.js
+++ loop.js
@@ -102,6 +102,56 @@ do {
   index++;
 } while (index < studentsData.length);
 
+// Unique Student Grades Tracker
+const students = [
+  { name: "Jenna", grades: { math: 90, english: 85, science: 88 } },
+  { name: "Mika", grades: { math: 75, history: 80, english: 90 } },
+  { name: "Liam", grades: { science: 82, history: 78, math: 88 } }
+];
+
+const uniqueSubject = new Set();
+const averageGrade = new Map();
+
+for (let student of students) {
+  for (let subject of Object.keys(student.grades)) {
+    uniqueSubject.add(subject);
+  }
+
+  const grades = Object.values(student.grades);
+  const average = grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
+  averageGrade.set(student.name, Math.floor(average));
+}
+
+console.log("Unique Subjects:", uniqueSubject);
+console.log("Average Grades:", averageGrade);
+
+
+// Product Sales Tracker
+const sales = [
+  { customer: "Jenna", product: "Laptop", quantity: 1 },
+  { customer: "Mika", product: "Phone", quantity: 2 },
+  { customer: "Jenna", product: "Phone", quantity: 1 },
+  { customer: "Liam", product: "Tablet", quantity: 3 },
+  { customer: "Mika", product: "Laptop", quantity: 1 },
+  { customer: "Liam", product: "Phone", quantity: 2 }
+];
+
+const uniqueProducts = new Set();
+const customerDetails = new Map();
+
+for (let sale of sales) {
+  uniqueProducts.add(sale.product);
+
+  if (!customerDetails.has(sale.customer)) {
+    customerDetails.set(sale.customer, sale.quantity);
+  } else {
+    const current = customerDetails.get(sale.customer);
+    customerDetails.set(sale.customer, current + sale.quantity);
+  }
+}
+
+console.log("Unique Products:", [...uniqueProducts]);
+console.log("Customer Details:", customerDetails);
 
 
 
@@ -190,7 +240,7 @@ const users = {
 const activeUsers = Object.values(users)
   .filter(user => user.active)
   .map(user => user.name);
-console.log("Active Users: ", activeUsers );
+console.log("Active Users: ", activeUsers);
 
 
 // Average Score Calculator
@@ -273,7 +323,7 @@ for (let [key, value] of map) {
 
 
 // JSON LOOP
- 
+
 
 // ✅ Convert to object
 const parsed = JSON.parse(jsonString);
@@ -384,8 +434,8 @@ let literPerSecond = 5;
 
 while (currentLevel + literPerSecond <= tankLiters) {
   currentLevel += literPerSecond;
-  console.log("Added "+ literPerSecond +"L. Current level: " + currentLevel +"L");
-} 
+  console.log("Added " + literPerSecond + "L. Current level: " + currentLevel + "L");
+}
 console.log("Tank is full");
 
 // Vending Machine Purchase
@@ -424,7 +474,7 @@ do {
     console.log("Login successful");
     success = true;
     break;
-  } 
+  }
   i++;
 } while (i < 3);
 
@@ -444,7 +494,7 @@ let house = 0;
 while (water >= houseWater) {
   water -= houseWater;
   house++
-  console.log("Delivered to House #" + house + ". Water Left: " + water + "L" )
+  console.log("Delivered to House #" + house + ". Water Left: " + water + "L")
 }
 
 // Social Media Scroll
@@ -469,3 +519,208 @@ while (batteryLevel - batteryPerScroll >= 10) {
 // Each minute, they type 7 words.
 // Simulate the typing and show how many words are left per minute.
 
+
+// Analyze Sales Orders
+const orders = [
+  {
+    orderId: 101,
+    customer: 'Jenna',
+    items: [
+      { product: 'Laptop', quantity: 1, price: 50000 },
+      { product: 'Mouse', quantity: 2, price: 500 }
+    ],
+    isPaid: true
+  },
+  {
+    orderId: 102,
+    customer: 'Kaye',
+    items: [
+      { product: 'Monitor', quantity: 1, price: 10000 },
+      { product: 'Mouse', quantity: 1, price: 500 }
+    ],
+    isPaid: false
+  },
+  {
+    orderId: 103,
+    customer: 'Jenna',
+    items: [
+      { product: 'Keyboard', quantity: 1, price: 1500 },
+      { product: 'Mouse', quantity: 1, price: 500 }
+    ],
+    isPaid: true
+  }
+];
+
+// 1. Total amount spent by Jenna
+const jennaTotal = orders
+  .filter(order => order.customer === 'Jenna' && order.isPaid)
+  .reduce((total, order) => {
+    const orderTotal = order.items.reduce((sum, item) => sum + item.quantity * item.price, 0);
+    return total + orderTotal;
+  }, 0);
+console.log('Jenna total:', jennaTotal);
+
+// 2. Unique products
+const uniqueItems = new Set();
+orders.forEach(order => {
+  order.items.forEach(item => {
+    uniqueItems.add(item.product);
+  });
+});
+console.log('Unique items:', [...uniqueItems]);
+
+// 3. Anyone ordered Laptop?
+const hasLaptop = orders.some(order =>
+  order.items.some(item => item.product === 'Laptop')
+);
+console.log('Has Laptop:', hasLaptop);
+
+// 4. Find first order with more than 1 Mouse
+const findOrder = orders.find(order =>
+  order.items.some(item => item.product === 'Mouse' && item.quantity > 1)
+);
+console.log('First order with >1 Mouse:', findOrder);
+
+// 5. Unpaid orders
+const notPaid = orders.filter(order => !order.isPaid);
+console.log('Unpaid orders:', notPaid);
+
+
+// ====================
+// Challenge: Employee Training Tracker
+const trainings = [
+  {
+    employee: "Jenna",
+    isCompleted: true,
+    courses: [
+      { title: "JavaScript Basics", hours: 10 },
+      { title: "React", hours: 12 }
+    ]
+  },
+  {
+    employee: "Mika",
+    isCompleted: false,
+    courses: [
+      { title: "UI/UX Design", hours: 8 },
+      { title: "JavaScript Basics", hours: 10 }
+    ]
+  },
+  {
+    employee: "Jenna",
+    isCompleted: true,
+    courses: [
+      { title: "Node.js", hours: 15 }
+    ]
+  },
+  {
+    employee: "Liam",
+    isCompleted: true,
+    courses: [
+      { title: "JavaScript Basics", hours: 10 },
+      { title: "TypeScript", hours: 14 }
+    ]
+  }
+];
+
+// Total training hours completed by Jenna
+const trainingHours = trainings
+  .filter(name => name.employee === 'Jenna' && name.isCompleted)
+  .reduce((total, training) => {
+    const hourTotal = training.courses.reduce((sum, courses) => sum + courses.hours, 0);
+    return total += hourTotal;
+  }, 0)
+console.log("Total Hours completed by Jenna: ", trainingHours);
+
+// List of all unique course titles taken (even if not completed)
+const uniqueCourse = new Set();
+trainings.forEach(training => {
+  training.courses.forEach(course => {
+    uniqueCourse.add(course.title);
+  })
+})
+console.log("Unique Course Title: ", [...uniqueCourse]);
+
+// Check if anyone took a course in “UI/UX Design”
+const hasDesignCourse = trainings.some(training => training.courses.some(course => course.title === 'UI/UX Design'));
+console.log("Has anyone took UI/UX Design? ", hasDesignCourse);
+
+// Find the first training where someone took more than 1 course
+const moreCourse = trainings.find(training => training.courses.length > 1);
+console.log("Training with more than 1 course: ", moreCourse);
+
+// List all trainings that are not completed
+const notCompleted = trainings.filter(training => !training.isCompleted);
+console.log("Trainings that are not completed: ", notCompleted);
+
+
+
+
+// ===============================
+// Student Exam Performance Tracker
+
+const studentExams = [
+  {
+    name: "Jenna",
+    exams: [
+      { subject: "Math", score: 88 },
+      { subject: "English", score: 92 },
+      { subject: "Science", score: 76 }
+    ]
+  },
+  {
+    name: "Mika",
+    exams: [
+      { subject: "Math", score: 59 },
+      { subject: "English", score: 61 },
+      { subject: "Science", score: 70 }
+    ]
+  },
+  {
+    name: "Liam",
+    exams: [
+      { subject: "Math", score: 95 },
+      { subject: "English", score: 85 },
+      { subject: "Science", score: 89 }
+    ]
+  }
+];
+
+// Average score per student
+const averages = studentExams
+  .map(student => {
+    const total = student.exams.reduce((sum, exam) => sum + exam.score, 0);
+    const average = total / student.exams.length;
+    return { name: student.name, average };
+});
+
+// List of all subjects taken (unique)
+const uniqueSubjectTaken = new Set();
+studentExams.forEach(student => {
+  student.exams.forEach(exam => {
+    uniqueSubjectTaken.add(exam.subject);
+  })
+});
+console.log("List of all subjects taken: ", uniqueSubjectTaken);
+
+// Find top scorer in Math
+let topScorerInMath = { name: "", score: -Infinity };
+
+studentExams.forEach(student => {
+  const mathExam = student.exams.find(exam => exam.subject === 'Math');
+  if (mathExam && mathExam.score > topScorerInMath.score) {
+    topScorerInMath = { name: student.name, score: mathExam.score };
+  }
+});
+console.log("Top Scorer in Math", topScorerInMath);
+
+// Total score in each subject (across all students)
+const subjectTotals = {};
+studentExams.forEach(student => {
+  student.exams.forEach(exam => {
+    if(!subjectTotals[exam.subject]) {
+      subjectTotals[exam.subject] = 0;
+    }
+    subjectTotals[exam.subject] += exam.score;
+  })
+});
+console.log("Total score per subject:", subjectTotals); 
